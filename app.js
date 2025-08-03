bde1398a1074b4c8e1f69fdc9ba823e602b880d3
Mungara-frency <frencyadam9493@gmail.com>
2025-08-03 09:40:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import { configDotenv } from 'dotenv';
+import connectToDb from './config/db.config.js';
+import errorMiddleware from './middleware/error.middleware.js';
+import userRoutes from './routes/user.routes.js'; 
+import courseRoutes from './routes/course.routes.js'; 
+import paymentRoutes from './routes/payment.routes.js';
+import miscellaneousRoutes from './routes/miscellaneous.routes.js';
+
+const app = express();
+configDotenv();
+
+app.use((req, res, next) => {
+  const allowedOrigin = process.env.CLIENT_URL;
+  console.log('Allowed Origin:', allowedOrigin);
+
+  res.header('Access-Control-Allow-Origin', allowedOrigin);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+
+  // Respond to preflight requests
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
+
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan('dev'));
+
+// Routes
+app.use("/user", userRoutes);
+app.use('/courses', courseRoutes);
+app.use('/payments', paymentRoutes);
+app.use('/', miscellaneousRoutes);
+
+// Handle 404 errors
+app.all('*', (req, res) => {
+  res.status(404).send('OOPS!! 404 page not found');
+});
+
+// Error middleware
+app.use(errorMiddleware);
+
+// Initialize the database
+connectToDb();
+
+export default app;
